72e5f9899669eba1392dfea4932c3ce7b62ac492
JMCRyGuy <68404993+JMCRyGuy@users.noreply.github.com>
2021-04-10 06:09:20 +1000

    From Class
    Moving Platforms, Rotation Script, Pro-builder assets and start of a level

---
+++ AGA307_Assessment2_TimeRush/Assets/!MadeAssets/Scripts/MovingPlatform.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<Transform> platformStops;
+    public int nextStop;
+    public float delay;
+    public float speed;
+    bool isWaiting = false;
+    
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaiting)
+            return;
+
+        // distance between points is less than an amount
+        if ((platformStops[nextStop].position - transform.position).magnitude < 0.1f )
+        //if (transform.position == platformStops[nextStop].position)
+        {
+
+            Invoke("FindNextStop", delay);
+            isWaiting = true;
+        }
+        else
+        {
+            MovePlatform();
+        }
+
+
+
+        
+    }
+
+    void MovePlatform()
+    {
+        Vector3 moving = platformStops[nextStop].position - transform.position;
+        transform.position += (moving / moving.magnitude) * speed * Time.deltaTime;
+    }
+    void FindNextStop()
+    {
+        nextStop++;
+        nextStop = nextStop % platformStops.Count;
+        isWaiting = false;
+    }
+
+
+
+}
